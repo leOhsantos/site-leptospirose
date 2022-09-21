d940b26b2f11afa9278a44cd2a27bbbd56c43610
leOhsantos <leonardo.santos191004@gmail.com>
2022-09-21 14:06:17 -0300

    atualizando animações

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ function handleMenuButton() {
 }
 
 function animateElements() {
-    const windowTop = window.pageYOffset + (window.innerHeight * 0.96);
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
 
     target.forEach(e => {
         if (windowTop > e.offsetTop) {
